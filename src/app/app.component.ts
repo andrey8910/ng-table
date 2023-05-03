d50ffe9e9c2bf03920458782c8d6116b9b9d30d0
ivanov6444 <a.ivanov6444@gmail.com>
2023-05-03 15:12:50 +0300

    create table sort data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {TableDataService} from "./services/table-data.service";
+import {Observable} from "rxjs";
+import {ProductData} from "./interfaces/product-data";
+import {UsersData} from "./interfaces/users-data";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-table';
+
+  showProductsNotFound = false;
+  allProductsData$: Observable<ProductData[]>;
+  allUsersData$: Observable<UsersData[]>;
+
+  constructor(
+    private tableDataService: TableDataService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.tableDataService.loadAllProducts();
+    this.tableDataService.loadAllUsers();
+    this.allProductsData$ = this.tableDataService.allProductsData$
+    this.allUsersData$ = this.tableDataService.allUsersData$
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TableDataService} from "./services/table-data.service";
+import {HttpClientModule} from "@angular/common/http";
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TableDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
